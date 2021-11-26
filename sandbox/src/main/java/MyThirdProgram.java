fcc1bf9a1924c4295b89adda268b2a9b008f2154
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-11-26 23:58:20 +0300

    Добавление метода расчета расстояния между точками

--- sandbox/src/main/java/MyThirdProgram.java
+++ sandbox/src/main/java/MyThirdProgram.java
@@ -5,10 +5,6 @@ public class MyThirdProgram {
     Point p1 = new Point(-5, 10.5);
     Point p2 = new Point(16, - 3.8);
 
-    System.out.println("Расстояние между двумя точками" + " = " + distance(p1,p2));
-  }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x-p1.x)+(p2.y-p1.y));
+    System.out.println("Расстояние между двумя точками" + " = " + p1.distance(p2.x,p2.y));
   }
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,8 +1,11 @@
 public class Point {
     public double x;
     public double y;
-    public Point(double x, double y) {
+    public Point (double x, double y) {
         this.x=x;
         this.y=y;
+    }
+    public double distance(double x, double y){
+        return Math.sqrt((x-this.x)+(y-this.y));
     }
 }
