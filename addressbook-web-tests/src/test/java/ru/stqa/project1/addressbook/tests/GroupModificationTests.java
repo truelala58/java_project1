470c64d41f7c8c520b4197b2bdc68efa3b1f2fbf
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-12-06 23:27:35 +0300

    тест на редактирование группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.project1.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.project1.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+        app.getSessionHelper().logout();
+    }
+}
