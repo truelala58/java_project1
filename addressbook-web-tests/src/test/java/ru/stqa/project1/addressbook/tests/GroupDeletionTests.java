67b5a078928f6beb3169f7154bfb2628de9c56e5
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-12-13 00:52:09 +0300

    проверка на создание группы

--- addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.project1.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.project1.addressbook.model.GroupData;
 
 
 public class GroupDeletionTests extends TestBase{
@@ -8,6 +9,9 @@ public class GroupDeletionTests extends TestBase{
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteGroup();
     app.getGroupHelper().returnToGroupPage();
