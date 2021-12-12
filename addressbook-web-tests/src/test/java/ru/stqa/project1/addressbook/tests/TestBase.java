6605ac96775d18096100ffa1026f28b5a97972f1
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-12-13 00:07:19 +0300

    оптимизация заполнения форм

--- addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.stqa.project1.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(Browser.FIREFOX.browserName());
+    protected final ApplicationManager app = new ApplicationManager(Browser.CHROME.browserName());
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
