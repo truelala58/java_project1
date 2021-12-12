3bef9e52cc79cea97335f3f74189e26df7f453b2
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-12-13 00:37:06 +0300

    проверки перехода на нужные страницы

--- addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/appmanager/NavigationHelper.java
@@ -10,9 +10,19 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+         return;
+        }
         click(By.linkText("groups"));
     }
     public void goToContactPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")
+                && isElementPresent(By.name("submit"))){
+            return;
+        }
         click(By.linkText("add new"));
     }
 
