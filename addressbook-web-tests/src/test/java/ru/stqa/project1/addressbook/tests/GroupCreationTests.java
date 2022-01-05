5f03c9277d422650538f34d7dfd9a3eb54320d02
truelala58 <55549323+truelala58@users.noreply.github.com>
2022-01-05 19:19:18 +0300

    вычисление максимального идентификатора

--- addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project1/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.project1.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,17 +14,11 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getCountList();
-    GroupData group = new GroupData("test1", "test2", "test3");
+    GroupData group = new GroupData("test2", "test2", "test3");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getCountList();
     Assert.assertEquals(after.size(),before.size() + 1);
-    int max = 0;
-    for (GroupData g: after){
-      if (g.getId()>max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     app.getSessionHelper().logout();
