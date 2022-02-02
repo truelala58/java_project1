20cdf73fe97a9892675cedd2e852355e4db3663c
truelala58 <55549323+truelala58@users.noreply.github.com>
2022-02-02 13:13:32 +0300

    интеграция тестов с Bugify:REST

--- rest-sample/src/test/java/ru/stqa/project1/rest/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/project1/rest/TestBase.java
@@ -13,6 +13,10 @@ import java.util.Set;
 
 public class TestBase {
 
+    private Executor getExecutor() {
+        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+
     public boolean isIssueOpen(int issueId) throws IOException {
         if ("Resolved".equals(getIssueStatus(issueId)) || ("Closed".equals(getIssueStatus(issueId)))) {
             return false;
@@ -26,17 +30,13 @@ public class TestBase {
         }
     }
 
-    private Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
-    }
-
     public String getIssueStatus(int issueId) throws IOException {
         String json = getExecutor().execute(Request.Get(String.format("https://bugify.stqa.ru/api/issues/%s.json", issueId)))
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
-        Set<Issue> issueParams = new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
-        }.getType());
+        Set<Issue> issueParams = new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
         return issueParams.iterator().next().getStateName();
     }
+
 }
